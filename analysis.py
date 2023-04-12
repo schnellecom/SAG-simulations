4bc25f2840a56bfc422de91dfe51e829126b2eb5
Lukas Schnelle <schnelle.com@gmail.com>
2023-04-12 19:33:50 +0200

    initial function designs

--- analysis.py
+++ analysis.py
@@ -1,13 +1,47 @@
-# i = 1
-# with open("data/rcv1_train.binary", "rb") as f:
-#     while (byte := f.read(1)) and i < 10:
-#         print(byte)
-#         i = i+1
-        # Do stuff with byte.
+import random
+
+import numpy
 import numpy as np
 import pandas as pd
-# np.fromfile("data/rcv1_train.binary")
-data = pd.read_csv('data/phy_train.dat', sep='\t', header=None, usecols=[i for i in range(1,80)])
+import matplotlib
+from matplotlib import pyplot as plt
+
+data = pd.read_csv('data/phy_train.dat', sep='\t', header=None, usecols=[i for i in range(2, 80)])
 data = data.transpose()
 
-print(data[2])
+target = pd.read_csv('data/phy_train.dat', sep='\t', header=None, usecols=[1])
+target = target.transpose()
+
+# print(data)
+# print(target)
+dim = target.size
+n = data.shape[1]
+
+
+def sag(iter):
+    x = 0
+    d = 0
+    y = []
+    for i in range(1, n):
+        y.append(numpy.zeros(dim))
+    plotdata = []
+
+    for k in range(1, iter):
+        i = random.randint(1, n)
+
+        plotdata.append(i)
+    plt.plot(range(1, iter), plotdata)
+    plt.show()
+    return x
+
+
+def f(i, x):
+    res = 0
+    for j in range(1, i):
+        res += x[j] * data[i][j]
+    res -= target[i]
+    res *= res
+    return res
+
+
+sag(20)
